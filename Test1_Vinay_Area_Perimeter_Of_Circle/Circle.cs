321937b0d8e44f87176c2ad2fdfbf9c8721a8da5
Rintu <Rintu@VINDUJA>
2022-11-05 15:42:23 +0530

    Add project files.

---
+++ Test1_Vinay_Area_Perimeter_Of_Circle/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_Vinay_Area_Perimeter_Of_Circle
+{
+    public class Circle
+    {
+        double pi = 3.14;
+        double AreaOfCircle;
+        double Perimeter;
+        public int Radius { get; set; }       
+        public void GetCircleAndPerimeter()
+        {
+            AreaOfCircle=pi * Radius * Radius;
+            Console.WriteLine("Area Of the Circle  : "+AreaOfCircle);
+            Perimeter=2*pi*Radius;
+            Console.WriteLine("Perimeter of the Circle  : "+Perimeter);
+
+        }
+    }
+}
---
+++ Test1_Vinay_Area_Perimeter_Of_Circle/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test1_Vinay_Area_Perimeter_Of_Circle;
+
+Circle objCircle = new Circle();
+
+Console.WriteLine("Enter Radius");
+objCircle.Radius = int.Parse(Console.ReadLine());
+objCircle.GetCircleAndPerimeter();
+
+
